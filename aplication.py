3e080e945804ae2c378abb2cd12aa6b1649614d2
Walter Vodeb <walter@conexiones.com>
2025-10-26 22:49:05 -0300

    a prompt as an agent.

--- aplication.py
+++ aplication.py
@@ -1,27 +1,43 @@
 
+import json
+from openai import OpenAI
+
+
 def OpenFile(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             file_content_as_string = file.read()
-        print("File content successfully read into a string:")
-        print(file_content_as_string)
+            return file_content_as_string
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    return file_content_as_string
+    return None
+
+
+def save_file(data, filename, indent=4):
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(data)
+        algo = OpenFile(filename) 
+        elJson = json.loads(algo)
+        return elJson
+        print(f"Data successfully saved to {filename}")
+    except IOError as e:
+        print(f"Error saving file {filename}: {e}")
+    except TypeError as e:
+        print(f"Error serializing data to JSON: {e}. Ensure all data types are JSON serializable.")
 
 
 
-from openai import OpenAI
 client = OpenAI()
 
-string_input = OpenFile("/Users/waltervodeb/Documents/Stanford/CoreNLP/docs/texto_1.txt")
+string_input = OpenFile("/Users/waltervodeb/Documents/Stanford/CoreNLP/docs/train_partes_walter_1.txt")
 
 response = client.responses.create(
   prompt={
     "id": "pmpt_68fd011996108197b5cf31467ce43e4f04844f18c0b88f21",
-    "version": "2"
+    "version": "8"
   },
   input=string_input,
   reasoning={},
@@ -32,6 +48,9 @@ response = client.responses.create(
   ]
 )
 
-print(response.output_text)
+if response:
+    save_file(response.output_text, "response_output.json")
+
+# print(response.output_text)
 
 
